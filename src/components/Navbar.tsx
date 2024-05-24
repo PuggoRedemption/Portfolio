bf9de829d1a6812e8c54ffc0744c0b316c7902a6
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-05-24 16:12:14 -0400

    Pulled Necessary SCSS File and Cleaned the Navbar.tsx File.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,10 @@
-import SidebarMenu, { SidebarMenuBody, SidebarMenuHeader, SidebarMenuNav, SidebarMenuNavLink, SidebarMenuNavTitle } from 'react-bootstrap-sidebar-menu';
+import Sidebar from "react-bootstrap-sidebar-menu";
 
 export default function () {
     return (
-        <SidebarMenu className='sidebar'>
-            <SidebarMenuHeader>
-                <h1>Jaeger Staeven</h1>
-            </SidebarMenuHeader>
-            <SidebarMenuBody>
-                <SidebarMenuNav>
-                    <SidebarMenuNavLink>
-                        <SidebarMenuNavTitle>About</SidebarMenuNavTitle>
-                    </SidebarMenuNavLink>
-                    <SidebarMenuNavLink>
-                        <SidebarMenuNavTitle>Projects</SidebarMenuNavTitle>
-                    </SidebarMenuNavLink>
-                    <SidebarMenuNavLink>
-                        <SidebarMenuNavTitle>Contact</SidebarMenuNavTitle>
-                    </SidebarMenuNavLink>
-                </SidebarMenuNav>
-            </SidebarMenuBody>
-        </SidebarMenu>
+        <Sidebar>
+            
+        </Sidebar>
     );
     
 }
