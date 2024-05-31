34f0b4a5ec9c33ae2d6b267a0eccbc749be3afdc
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-05-31 15:56:05 -0400

    Added Animations to About Component
    Added translate and fade-in animations to the About component text.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,8 @@
 export default function About() {
     return (
-        <p>About</p>
+        <div>
+            <h1 className="animation-open delay-1">My name is Jaeger.</h1>
+            <h1 className="animation-open delay-2">I'm a Web Developer.</h1>
+        </div>
     );
 }
