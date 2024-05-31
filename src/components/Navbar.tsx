ce73b8f5eb99f2a1b4670daa2f59e9d63ffedda7
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-05-31 12:25:53 -0400

    Update Navbar Content
    Added some small changes to the format of the Navbar to be a little neater (tabbed sublinks, thumbnail, centered headers). Also added a thumbnail image to the project.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Sidebar from "react-bootstrap-sidebar-menu";
+import thumbnail from "../assets/thumbnail.jpg";
 
+// Set up SideBar Navigation Content as Links
 export default function () {
     return (
         <Sidebar expand="md">
             <Sidebar.Collapse getScrollValue={500}>
-                <Sidebar.Header>
+                <Sidebar.Header id="Name">
                     <Sidebar.Brand>Jaeger Staeven</Sidebar.Brand>
+                </Sidebar.Header>
+                <Sidebar.Header id="Thumbnail">
+                    <Sidebar.Brand><img src={thumbnail} alt="thumbnail"/></Sidebar.Brand>
                 </Sidebar.Header>
                 <Sidebar.Body>
                     <Sidebar.Nav>
@@ -21,12 +26,12 @@ export default function () {
                         <Sidebar.Nav.Link eventKey={"menu_contact"}>
                             <Sidebar.Nav.Title>Contact</Sidebar.Nav.Title>
                         </Sidebar.Nav.Link>
-                        <Sidebar.Sub>
+                        <Sidebar.Nav.Link className="sub-link">
                             <Sidebar.Nav.Title>Email</Sidebar.Nav.Title>
-                        </Sidebar.Sub>
-                        <Sidebar.Sub>
+                        </Sidebar.Nav.Link>
+                        <Sidebar.Nav.Link className="sub-link">
                             <Sidebar.Nav.Title>LinkedIn</Sidebar.Nav.Title>
-                        </Sidebar.Sub>
+                        </Sidebar.Nav.Link>
                     </Sidebar.Nav>
                 </Sidebar.Body>
             </Sidebar.Collapse>
