10b4897dd0e444503c78a9d82c495dc575700aad
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-06-01 11:30:46 -0400

    Basic Contact Form Structure
    Created a contact form with some styling. It has no current functionality yet.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,14 @@
 export default function Contact() {
     return (
-        <p>Contact</p>
+        <div className="contact-form">
+            <h1>Email Me</h1>
+            <h3>Full Name</h3>
+            <input type="text" name="Full Name" required></input>
+            <h3>Email</h3>
+            <input type="email" name="email" required></input>
+            <h3>Message</h3>
+            <textarea rows={5} cols={60} name="message" autoComplete="off" required></textarea>
+            <button type="submit">Send Message</button>
+        </div>
     );
 }
