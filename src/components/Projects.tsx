4c98898fb07cb0770880bb55a4ea0ab6fa5b979b
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-06-03 13:13:17 -0400

    Added A Card Template to Project Component
    Created a card template in the Project component. Cards will show a picture of the product and have a small overlay to provide quick information to the user when they hover over the card. The cards are clickable, opening a link to the project in a new tab.
    
    Also added an image of my first project (Shardrun) to use for the card.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,19 @@
+import Card from "react-bootstrap/Card";
+import Image from "../assets/Shardrun.jpg";
+
+//https://siyahas.itch.io/shardrun
+
+// Project Cards
 export default function Projects() {
     return (
-        <p>Projects</p>
+        <Card className="bg-dark mx-auto text-white project-card">
+            <a href="https://siyahas.itch.io/shardrun" target="_blank" rel="noopener noreferrer">
+                <Card.Img src={Image}/>
+                <Card.ImgOverlay className="card-overlay d-flex flex-column">
+                    <Card.Title className="align-items-center mx-auto mt-auto card-title">ShardRun</Card.Title>
+                    <Card.Text className="align-items-center mx-auto mb-auto card-text">Test Details</Card.Text>
+                </Card.ImgOverlay>
+            </a>
+        </Card>
     );
 }
