72391cb7ed25f244126464b930ceb69f0bed548b
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-06-07 11:46:00 -0400

    Pulled Contact Form out of Component
    Removed the contact form from a component and placed it in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import About from "./components/About";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
@@ -16,7 +16,19 @@ export default function App() {
       <main className="main-container">
         <About />
         <Projects />
-        <Contact />
+        {/* <Contact /> */}
+        <div className="contact-form">
+            <form name="contact" method="POST" data-netlify="true">
+                <h1>Email Me</h1>
+                <h3>Full Name</h3>
+                <input type="text" name="Full Name" required></input>
+                <h3>Email</h3>
+                <input type="email" name="email" required></input>
+                <h3>Message</h3>
+                <textarea rows={5} cols={60} name="message" autoComplete="off" required></textarea>
+                <button type="submit">Send Email</button>
+            </form>
+        </div>
       </main>
     </div>
   );
