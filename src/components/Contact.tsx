64d3d1da199c80231dedddd979e1f3bcbdc79d2d
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-06-07 11:18:52 -0400

    Test Deploy
    Test deploy to see if "netlify" works to connect the contact form.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@
 export default function Contact() {
     return (
         <div className="contact-form">
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" method="POST" netlify>
                 <h1>Email Me</h1>
                 <h3>Full Name</h3>
                 <input type="text" name="Full Name" required></input>
