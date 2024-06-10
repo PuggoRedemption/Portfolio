b353bcb430ee092a58bb477d954f096262808669
PuggoRedemption <jaegerstaeven@yahoo.com>
2024-06-10 11:18:16 -0400

    Styling Changes
    Centered the About component text. Started to work some responsive sidebar styles.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 // Introduction
 export default function About() {
     return (
-        <div>
+        <div className="about-text">
             <h1 className="animation-open delay-1">My name is Jaeger.</h1>
             <h1 className="animation-open delay-2">I'm a Web Developer.</h1>
         </div>
